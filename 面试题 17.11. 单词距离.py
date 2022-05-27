012e87f87d420ba9841dc4748b61250daf32b84a
wangxshuai <1940692628@qq.com>
2022-05-27 22:24:22 +0800

    Update 面试题 17.11. 单词距离.py

--- 面试题 17.11. 单词距离.py
+++ 面试题 17.11. 单词距离.py
@@ -10,7 +10,6 @@ class Solution:
         res = 100000000
         len2 = len(cnt2)
         for item in cnt1:
-            tmpres = 100000000
             start = 0 
             end = len2-1
             mid = (start+end)//2
@@ -21,11 +20,7 @@ class Solution:
                     end = mid
                 mid = (start+end)//2
             if words[cnt2[mid]]==word2:
-                tmpres = min(tmpres,abs(cnt2[mid]-item))
+                res = min(res,abs(cnt2[mid]-item))
             if mid-1>-1 and words[cnt2[mid-1]]==word2:
-                tmpres = min(tmpres,abs(cnt2[mid-1]-item))
-            if tmpres == 100000000:
-                tmpres = min(abs(cnt2[0]-item),abs(cnt2[len2-1]-item))
-            res = min(res,tmpres)
+                res = min(res,abs(cnt2[mid-1]-item))
         return res
-            
